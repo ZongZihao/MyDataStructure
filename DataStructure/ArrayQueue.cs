44e9cd8b1eadfc9bc0f5ce0a367ff9a4ed06053f
unknown <915637831@qq.com>
2018-10-16 22:01:23 +0800

    数组和链表实现队列

---
+++ DataStructure/ArrayQueue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    //顺序队列
+    class ArrayQueue<T>
+    {
+        //数组items， 数组大小n
+        private T[] items;
+        private int n;
+        //队头和队尾下标
+        int head = 0;
+        int tail = 0;
+
+        //初始化申请一个大小为capacity的数组
+        public ArrayQueue(int capacity)
+        {
+            items = new T[capacity];
+            n = capacity;
+        }
+
+        //入队
+        public bool Enqueue(T item)
+        {
+            //若队列已满
+            if (tail == n)
+            {
+                if (head == 0) return false;
+                //数据搬移
+                for(int i = head; i < tail; i++)
+                {
+                    items[i - head] = items[i];
+                }
+                tail -= head;
+                head = 0;
+            }
+
+            items[tail] = item;
+            tail++;
+            return true;
+        }
+
+        //出队
+        public T dequeue()
+        {
+            //如果队列为空
+            if (head == tail) return default(T);
+            T res = items[head];
+            head++;
+            return res;
+        }
+    }
+}
---
+++ DataStructure/LinkedQueue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    class LinkedQueue
+    {
+
+        //队列的首尾
+        private Node head = null;
+        private Node tail = null;
+
+        //入队
+        public void Enqueue(string value)
+        {
+            if(tail == null)
+            {
+                Node newNode = new Node(value, null);
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                Node newNode = new Node(value, null);
+                tail.next = newNode;
+                tail = tail.next;
+            }
+        }
+
+        //出队
+        public string Dequeue()
+        {
+            if (head == null) return null;
+            string res = head.data;
+            head = head.next;
+            if (head == null)
+                tail = null;
+            return res;
+        }
+
+        private class Node
+        {
+            public string data;
+            public Node next;
+
+            public Node(string data, Node next)
+            {
+                this.data = data;
+                this.next = next;
+            }
+
+            public string GetData()
+            {
+                return data;
+            }
+        }
+
+    }
+
+    
+}
